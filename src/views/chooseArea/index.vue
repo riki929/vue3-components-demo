b9e084b750091ab4fd633bc398a1d404685591fa
riki929 <riki929@outlook.com>
2021-12-16 17:33:52 +0800

    增加省市区三级联动组件

---
+++ src/views/chooseArea/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <choose-area @change="changeArea"></choose-area>
+</template>
+
+<script lang="ts" setup>
+import ChooseArea from "../../components/chooseArea/src/index.vue";
+
+let changeArea = (val: any) => {
+  console.log(val);
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
